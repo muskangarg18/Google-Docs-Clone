0ef40d64edbd300079887a5a5c8702c7858be291
muskan <muskangargmg7@gmail.com>
2023-05-28 11:51:59 +0530

    latest commit

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,9 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
 
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
-  console.log(__dirname);
-
-  return {
-    plugins: [react()],
-    define: {
-      __APP_ENV__: JSON.stringify(env.APP_ENV)
-    },
-    root: 'src',
-    build: {
-      outDir: '../dist',
-      rollupOptions: {
-        input: {
-          main: resolve(__dirname, 'src/main.jsx'), // Specify the JavaScript entry file here
-          
-        }
-      }
-    }
-  };
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
 });
