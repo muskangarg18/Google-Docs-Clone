f372d929173e4707ee0671e98b762e0f573c7cd0
muskan <muskangargmg7@gmail.com>
2023-05-28 11:03:36 +0530

    Merge remote changes

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
 import { Outlet } from 'react-router-dom';
-import Header from './header';
+import Header from './Header';
 
 const theme= createTheme({
     palette:{
